5f0d962ba5f439244ad497f5874f19a03764293a
Vasil Kosturski <v.kosturski@synergysportstech.com>
2021-08-25 16:37:42 +0300

    Use a concrete version of the tensorflow/serving image

--- src/deploy-model.bat
+++ src/deploy-model.bat
@@ -1,2 +1,2 @@
-docker pull tensorflow/serving
+docker pull tensorflow/serving:2.6.0
 docker run -t --rm --name=tf-serving-mnist -p 8500:8500 -v "%cd%/mnist-model:/models/model" tensorflow/serving --model_name=mnist-model
